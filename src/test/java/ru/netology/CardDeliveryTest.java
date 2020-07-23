cf21d5645904b6ae84dec54acead3ea42d767efb
Ander1977 <kopa.andrej@gmail.com>
2020-07-23 08:50:16 +0300

    Fix project

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,17 +1,18 @@
+package ru.netology;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.DateUtils;
-import ru.netology.RegistrationByNamePhone;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.chord;
 import static ru.netology.DataGenerator.Registration.generateByNamePhone;
+import static ru.netology.DateUtils.localDateTime;
 
 public class CardDeliveryTest {
     RegistrationByNamePhone registrationData = generateByNamePhone("RU");
-    private final String currentDate = DateUtils.localDateTime();
+    private final String currentDate = localDateTime();
     private final String selectAll = chord(Keys.CONTROL, "a");
     private final Keys del = Keys.DELETE;
 
@@ -105,6 +106,19 @@ public class CardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
         $(".input_invalid[data-test-id='city']").shouldHave(text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldPhoneInputNotCorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").sendKeys(selectAll, del);
+        $("[data-test-id='date'] input").setValue(currentDate);
+        $("[data-test-id='name'] input").setValue(registrationData.getFullName());
+        $("[data-test-id='phone'] input").setValue("+");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
 }
